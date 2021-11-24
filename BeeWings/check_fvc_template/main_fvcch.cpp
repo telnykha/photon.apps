2128e35ada7d1d9c1e2cdc370d6061ee25296ca9
NN-Videolab <telnykha@yahoo.com>
2021-11-24 18:41:50 +0300

    BeeWings add FVCUtils

---
+++ BeeWings/check_fvc_template/main_fvcch.cpp
@@ -0,0 +1,137 @@
+#pragma hdrstop
+#pragma argsused
+
+#ifdef _WIN32
+#include <tchar.h>
+#else
+  typedef char _TCHAR;
+  #define _tmain main
+#endif
+
+#include <stdio.h>
+
+#include <io.h>
+#include <string>
+
+#include "_LF.h"
+#include "fvlcore.h"
+
+extern "C"
+{
+	#pragma link "awpipl2b.lib"
+	#pragma link "JPEGLIB.lib"
+
+}
+#pragma link "TinyXML.lib"
+#pragma link "awplflibb.lib"
+
+ void Usage()
+{
+	printf("Usage: fvccheck fvccheck.xml\n");
+}
+ int _tmain(int argc, _TCHAR* argv[])
+{
+ 	if (argc < 2)
+	{
+		Usage();
+		return 1;
+	}
+
+	// main program
+	TiXmlDocument doc;
+	if (!doc.LoadFile(argv[1]))
+	{
+		printf("error: cannot load configuration.\n");
+		return 1;
+	}
+
+	TiXmlHandle hDoc(&doc);
+	TiXmlElement* pElem = NULL;
+	pElem = hDoc.FirstChildElement().Element();
+	if (!pElem)
+	{
+		printf("error: invalid configuration file.\n");
+		return 1;
+	}
+   const char* name = pElem->Value();
+   if (strcmp(name, "fvcccheck") != 0)
+	{
+		printf("error: invalid configuration file.\n");
+		return 1;
+	}
+
+	string path 	     =  pElem->Attribute("in_database");
+	string outName       =  pElem->Attribute("out_name");
+	string tmplName      =  pElem->Attribute("tmpl_name");
+
+
+	printf("params:\n");
+
+	printf("input database: %s\n", path.c_str());
+	printf("out name: %s\n", outName.c_str());
+	printf("tmpl name: %s\n", tmplName.c_str());
+
+	FvcTemplate* tmpl = NULL;
+	if (fvcLoadTemplate(tmplName.c_str(), &tmpl) != FVC_OK)
+	{
+        printf("error: cannot load template.\n");
+		return 1;
+	}
+
+	printf("template width %i:\n", tmpl->nVectorWidth);
+	printf("template height %i:\n", tmpl->nVectorHeight);
+	printf("template num vectors %i:\n", tmpl->nNumVectors);
+
+	_finddata_t filesInfo;
+	int num_images = 0;
+	long handle = 0;
+	FILE* f = fopen(outName.c_str(), "w+t");
+	if ( (handle = _findfirst( (char*)((path+"*.awp").c_str()), &filesInfo)) != -1 )
+	{
+		do
+		{
+			string name = path + filesInfo.name;
+			TLFImage image;
+			if (!image.LoadImage((char*)name.c_str()))
+			{
+				printf("cannot load image %s \n", name.c_str());
+				continue;
+			}
+			num_images++;
+			double ro = 0;
+
+			awpImage* img = image.GetImage();
+			awpConvert(img, AWP_CONVERT_TO_FLOAT);
+			fvcCompare(img, tmpl, &ro, FVC_COMPARE_EUCLID);
+			float ro1 = ro;
+			fprintf(f, "%s\t%f\n", filesInfo.name, ro1);
+/*			awpImage* rec = NULL;
+			if (fvcGetReconstruction(tmpl, img, &rec) != FVC_OK)
+				printf("reconstruction error.\n");
+			if (rec != NULL)
+			{
+				awpImage* norm = NULL;
+				awpNormalize(rec, &norm, AWP_NORM_L2);
+				awpConvert(norm, AWP_CONVERT_TO_FLOAT);
+				//
+				double ro = 0;
+				if (awpDistance(img, rec, AWP_DIST_L2 ,&ro) != AWP_OK)
+					printf("distance error.\n");
+				float ro1 = ro;
+				fprintf(f, "%s\t%f\n", filesInfo.name, ro1);
+				awpReleaseImage(&rec);
+				awpReleaseImage(&norm);
+			}*/
+
+			if (num_images % 100 == 0)
+				printf(">");
+
+		}while(!_findnext( handle, &filesInfo ));
+	}
+	_findclose( handle );
+	fclose(f);
+
+
+	printf("\ndone.\n");
+	return 0;
+}
---
+++ BeeWings/create_fvc_template/main_fvccr.cpp
@@ -0,0 +1,157 @@
+#pragma hdrstop
+#pragma argsused
+
+#ifdef _WIN32
+#include <tchar.h>
+#else
+  typedef char _TCHAR;
+  #define _tmain main
+#endif
+
+#include <stdio.h>
+
+#include <io.h>
+#include <string>
+
+#include "_LF.h"
+#include "fvlcore.h"
+
+extern "C"
+{
+	#pragma link "awpipl2b.lib"
+	#pragma link "JPEGLIB.lib"
+
+}
+#pragma link "TinyXML.lib"
+#pragma link "awplflibb.lib"
+
+void Usage()
+{
+	printf("Usage: fvccreate fvlcreate.xml\n");
+}
+
+ int _tmain(int argc, _TCHAR* argv[]) 
+{
+	if (argc < 2)
+	{
+		Usage();
+		return 1;
+	}
+
+	// main program
+	TiXmlDocument doc;
+	if (!doc.LoadFile(argv[1]))
+	{
+		printf("error: cannot load configuration.\n");
+		return 1;
+	}
+
+	TiXmlHandle hDoc(&doc);
+	TiXmlElement* pElem = NULL;
+	pElem = hDoc.FirstChildElement().Element();
+	if (!pElem)
+	{
+		printf("error: invalid configuration file.\n");
+		return 1;
+	}
+   const char* name = pElem->Value();
+   if (strcmp(name, "fvccreate") != 0)
+	{
+		printf("error: invalid configuration file.\n");
+		return 1;
+	}
+
+	string path 	     =  pElem->Attribute("in_database");
+	string outName       =  pElem->Attribute("out_name");
+	int width  = 24;
+	int height = 24;
+	int num_vct = 16;
+	pElem->QueryIntAttribute("width",  &width);
+	pElem->QueryIntAttribute("height",  &height);
+	pElem->QueryIntAttribute("num_vct",  &num_vct);
+
+	printf("params:\n");
+
+	printf("input database: %s\n", path.c_str());
+	printf("out name: %s\n", outName.c_str());
+	printf("width: %i\n", width);
+	printf("height: %i\n", height);
+	printf("num vectors: %i\n", num_vct);
+
+   FvcTemplate* pTemplate = NULL;
+
+	//string path( dirName );
+	string path1 = path + "\\*.awp";
+	_finddata_t filesInfo;
+	int  numFiles = 0;
+	long handle = 0;
+	if ( (handle = _findfirst( (char*)((path+"*.awp").c_str()), &filesInfo)) != -1 )
+	{
+		do
+		{
+			numFiles++;
+
+		}while(!_findnext( handle, &filesInfo ));
+	}
+	_findclose( handle );
+
+
+	if( !numFiles ){
+		cout << "Files not found" << endl;
+		return -1;
+	}
+	int numVect = num_vct;
+	awpImage** sourceImages = new awpImage* [numFiles];
+	handle = _findfirst((char*)((path+"*.awp").c_str()), &filesInfo );
+	string fileName;
+
+
+	for( int i=0; i<numFiles; i++  ){
+		//cout << filesInfo.name << endl;
+		cout << ".";
+		fileName = path + filesInfo.name;
+
+		AWPRESULT load_res = awpLoadImage( fileName.c_str(), &sourceImages[i] );
+		awpConvert(sourceImages[i], AWP_CONVERT_TO_FLOAT);
+		if( load_res )
+			cout << load_res;
+		if( sourceImages[i]->sSizeX != width && sourceImages[i]->sSizeY != height){
+			AWPRESULT resize_res = awpResize( sourceImages[i], width, height );
+			cout << resize_res;
+		}
+		_findnext( handle, &filesInfo );
+	}
+	int create_res = fvcCreateTemplate( &pTemplate, width, height, numFiles, true );
+	if( create_res != FVC_OK )
+		cout << create_res;
+	int build_res = fvcBuildTemplate(sourceImages, numFiles, pTemplate);
+	if( build_res )
+			cout << build_res;
+	if( numVect )
+		fvcResampleTemplate( pTemplate, numVect );
+	int save_res = fvcSaveTemplate( outName.c_str(), pTemplate );
+	if( save_res )
+		cout << save_res;
+	int n;
+	char* tmplFileName = (char*)outName.c_str();
+	string str( tmplFileName );
+	n = str.rfind( '.', str.length()-1 );
+	char * imageName = new char[ n+10 ];
+	memcpy( imageName, str.c_str(), n+1 );
+	memcpy( imageName+n+1, "jpg\0", 4 );
+	int saveIm_res = fvcSaveTemplateAsImage( imageName, pTemplate );
+	if( saveIm_res )
+		cout << saveIm_res;
+	for( int j = 0; j < numFiles; j++ )
+	{
+		if( sourceImages[j] != NULL )
+			awpReleaseImage( &sourceImages[j] );
+	}
+	fvcFreeTemplate( &pTemplate );
+	delete[] sourceImages;
+	delete[] imageName;
+
+	printf("done.\n");
+
+	return 0;
+}
